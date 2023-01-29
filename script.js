ba5182381934e1c08a7de0ad7f05820e32f5aae6
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-01-29 23:46:46 +0200

    Add popup form and transition style

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+const mainElem = document.querySelector(".main");
 const myLibrary = [];
+
+function addBookToDisplay() {
+  myLibrary.forEach((book) => {
+    const newBookElem = document.createElement("div");
+    const bookTitle = document.createElement("div");
+    bookTitle.textContent = `"${book.title}"`;
+    const bookAuthor = document.createElement("div");
+    bookAuthor.textContent = book.author;
+    const bookPages = document.createElement("div");
+    bookPages.textContent = book.pages;
+
+    newBookElem.classList.add("book");
+
+    newBookElem.appendChild(bookTitle);
+    newBookElem.appendChild(bookAuthor);
+    newBookElem.appendChild(bookPages);
+
+    mainElem.appendChild(newBookElem);
+  });
+}
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -21,43 +42,12 @@ function addBookToLibrary() {
 
   const newBook = new Book(book, author, pages, readFlag);
   myLibrary.push(newBook);
+  addBookToDisplay();
 }
+const popup = document.querySelector(".add-book-popup");
 
-const book1 = new Book("Book1", "author 1", 123, true);
-const book2 = new Book("Book2", "author 2", 123, true);
-const book3 = new Book("Book3", "author 3", 123, true);
-const book4 = new Book("Book4", "author 4", 123, true);
-const book5 = new Book("Book5", "author 5", 123, true);
-const book6 = new Book("Book6", "author 6", 123, true);
-
-// add book button
+// add book button JUST TESTING BELOW THAT IT WORKS...
 const addBookButton = document.querySelector(".add-new-book");
-
-addBookButton.addEventListener("click", addBookToLibrary);
-
-myLibrary.push(book1);
-myLibrary.push(book2);
-myLibrary.push(book3);
-myLibrary.push(book4);
-myLibrary.push(book5);
-myLibrary.push(book6);
-
-const mainElem = document.querySelector(".main");
-
-myLibrary.forEach((book) => {
-  const newBookElem = document.createElement("div");
-  const bookTitle = document.createElement("div");
-  bookTitle.textContent = `"${book.title}"`;
-  const bookAuthor = document.createElement("div");
-  bookAuthor.textContent = book.author;
-  const bookPages = document.createElement("div");
-  bookPages.textContent = book.pages;
-
-  newBookElem.classList.add("book");
-
-  newBookElem.appendChild(bookTitle);
-  newBookElem.appendChild(bookAuthor);
-  newBookElem.appendChild(bookPages);
-
-  mainElem.appendChild(newBookElem);
+addBookButton.addEventListener("click", () => {
+  popup.classList.add("open-popup");
 });
