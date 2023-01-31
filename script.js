08e46e991846f22c752af540d66a022d6f3f5811
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-02-01 00:24:58 +0200

    Add remove and read/not read buttons on book elems

--- script.js
+++ script.js
@@ -5,31 +5,110 @@ const readCheckbox = document.querySelector(".read-checkbox");
 const newBookSubmitButton = document.querySelector("#new-book-btn");
 const popup = document.querySelector(".add-book-popup");
 const filter = document.querySelector(".filter");
+const addBookButton = document.querySelector(".add-new-book");
 // vars
 const myLibrary = [];
+let index = 0;
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, index) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.index = index;
 }
 
 function showBook(book, index) {
   const newBookElem = document.createElement("div");
-  const bookTitle = document.createElement("div");
+  // book title
+  const bookTitleContainer = document.createElement("div");
+  const bookTitleLabel = document.createElement("span");
+  const bookTitle = document.createElement("span");
+  bookTitleLabel.textContent = "Title: ";
   bookTitle.textContent = `"${book.title}"`;
-  const bookAuthor = document.createElement("div");
-  bookAuthor.textContent = book.author;
-  const bookPages = document.createElement("div");
-  bookPages.textContent = book.pages;
+  bookTitleContainer.appendChild(bookTitleLabel);
+  bookTitleContainer.appendChild(bookTitle);
+  // author
+  const authorContainer = document.createElement("div");
+  const authorLabel = document.createElement("span");
+  const author = document.createElement("span");
+  authorLabel.textContent = "Author: ";
+  author.textContent = book.author;
+  authorContainer.appendChild(authorLabel);
+  authorContainer.appendChild(author);
+  // pages
+  const bookPagesContainer = document.createElement("div");
+  const pagesLabel = document.createElement("span");
+  const pages = document.createElement("span");
+  pagesLabel.textContent = "Pages: ";
+  pages.textContent = book.pages;
+  bookPagesContainer.appendChild(pagesLabel);
+  bookPagesContainer.appendChild(pages);
+  // read & remove buttons
+  const buttonContainer = document.createElement("div");
+  const readToggleButton = document.createElement("button");
+  const removeButton = document.createElement("button");
+  readToggleButton.classList.add("toggle-read-button");
+  if (book.read) {
+    readToggleButton.textContent = "Read";
+    readToggleButton.classList.add("bgc-read");
+  } else {
+    readToggleButton.textContent = "Not read";
+    readToggleButton.classList.add("bgc-not-read");
+  }
+  removeButton.textContent = "Remove";
+  removeButton.classList.add("remove-button");
+
+  // add event listeners for buttons
+  removeButton.addEventListener("click", () => {
+    console.log("HELLO");
+    const bookIndex = Number(
+      removeButton.parentElement.parentElement.getAttribute("data-index")
+    );
+    const currentBook = removeButton.parentElement.parentElement;
+    for (let i = 0; i < myLibrary.length; i++) {
+      if (myLibrary[i].index === bookIndex) {
+        myLibrary.splice(i, 1);
+        currentBook.remove();
+      }
+    }
+  });
+
+  readToggleButton.addEventListener("click", () => {
+    const bookIndex = Number(
+      readToggleButton.parentElement.parentElement.getAttribute("data-index")
+    );
+    for (let i = 0; i < myLibrary.length; i++) {
+      if (
+        myLibrary[i].index === bookIndex &&
+        readToggleButton.textContent === "Read"
+      ) {
+        myLibrary[i].read = false;
+        readToggleButton.classList.remove("bgc-read");
+        readToggleButton.classList.add("bgc-not-read");
+        readToggleButton.textContent = "Not read";
+      } else if (
+        myLibrary[i].index === bookIndex &&
+        readToggleButton.textContent === "Not read"
+      ) {
+        myLibrary[i].read = true;
+        readToggleButton.classList.remove("bgc-not-read");
+        readToggleButton.classList.add("bgc-read");
+        readToggleButton.textContent = "Read";
+      }
+    }
+  });
+
+  buttonContainer.appendChild(readToggleButton);
+  buttonContainer.appendChild(removeButton);
+  // book general
   newBookElem.classList.add("book");
   newBookElem.setAttribute("data-index", index);
-  newBookElem.appendChild(bookTitle);
-  newBookElem.appendChild(bookAuthor);
-  newBookElem.appendChild(bookPages);
+  newBookElem.appendChild(bookTitleContainer);
+  newBookElem.appendChild(authorContainer);
+  newBookElem.appendChild(bookPagesContainer);
+  newBookElem.appendChild(buttonContainer);
 
-  // TODO: add read/not-read toggle + remove button
   mainElem.appendChild(newBookElem);
 }
 
@@ -47,31 +126,35 @@ function createBook(e) {
   const read = readCheckbox.children.read.checked !== false;
   if (title !== "" && author !== "" && pages !== "") {
     e.preventDefault();
-    const newBook = new Book(title, author, pages, read);
+    const newBook = new Book(title, author, pages, read, index);
     myLibrary.push(newBook);
-    const index = myLibrary.length - 1;
+    // const index = myLibrary.length - 1;
     showBook(newBook, index);
     // close popup & clear form
     popup.classList.remove("open-popup");
     filter.classList.toggle("filter-opened");
+    index += 1;
     clearForm();
+    addBookButton.classList.remove("disableButtons");
   }
 }
 
 // open popup
-const addBookButton = document.querySelector(".add-new-book");
 addBookButton.addEventListener("click", () => {
+  addBookButton.classList.add("disableButtons");
   popup.classList.add("open-popup");
   filter.classList.toggle("filter-opened");
 });
 
 // close popup w/ click & w/ ESC
 filter.addEventListener("click", () => {
+  addBookButton.classList.remove("disableButtons");
   popup.classList.remove("open-popup");
   filter.classList.toggle("filter-opened");
 });
 window.addEventListener("keydown", (e) => {
   if (popup.classList[1] === "open-popup" && e.key === "Escape") {
+    addBookButton.classList.remove("disableButtons");
     popup.classList.remove("open-popup");
     filter.classList.toggle("filter-opened");
   }
